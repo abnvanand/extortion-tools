abnvanand/extortion-tools

commit 07c305f87fb47f7f2fd1995735c81635dbeed9aa

foreign_sched_values_calc: Fix form67 values for withholding > dtaa.

foreign_sched_values_calc/foreign_sched_values_calc.py
@@ -4460,10 +4460,6 @@ def create_schedule_fsi_and_form_67(avg_tax_rate: Fraction) -> None:
         income_value: Fraction,
         tax_withheld: Fraction,
     ) -> None:
-        int_tax_in_india = int(income_value * avg_tax_rate)
-        int_tax_withheld = int(tax_withheld)
-        int_credit_claimed = min(int_tax_in_india, int_tax_withheld)
-
         income_type_mapping = {
             "ltcg": "Long term capital gain",
             "stcg": "Short term capital gain",
@@ -4473,9 +4469,16 @@ def create_schedule_fsi_and_form_67(avg_tax_rate: Fraction) -> None:
         def country_attr(attr: str) -> Any:
             return getattr(country, f"{attr}_{income_type_key}")
 
-        # We do not support tax rate different from DTAA rate as of now.
-        tax_rate_pc_outside_india = format(
-            country_attr("dtaa_tax_rate_percent"), ".2f"
+        dtaa_tax_rate_pc = country_attr("dtaa_tax_rate_percent")
+        withholding_tax_rate_pc = country_attr("tax_withholding_rate_percent")
+
+        int_tax_in_india = int(income_value * avg_tax_rate)
+        int_tax_dtaa = int(income_value * dtaa_tax_rate_pc / 100)
+        int_tax_withheld = int(tax_withheld)  # Can potentially be > DTAA.
+
+        # Credit to claim must be the lowest >:(.
+        int_credit_claimed = min(
+            int_tax_in_india, int_tax_dtaa, int_tax_withheld
         )
 
         # Since column names are same, the stupid engineer chose a trailing
@@ -4496,7 +4499,7 @@ def create_schedule_fsi_and_form_67(avg_tax_rate: Fraction) -> None:
 
             "Income from outside India": int(income_value),
             "Amount": int_tax_withheld,
-            "Rate(%)": tax_rate_pc_outside_india,
+            "Rate(%)": format(withholding_tax_rate_pc, ".2f"),
 
             "Tax payable on such income under normal provisions in India":
                 int_tax_in_india,
@@ -4505,7 +4508,7 @@ def create_schedule_fsi_and_form_67(avg_tax_rate: Fraction) -> None:
             "Article No. of Double Taxation Avoidance Agreements":
                 country_attr("dtaa_article"),
             "Rate of tax as per Double Taxation Avoidance Agreements(%)":
-                tax_rate_pc_outside_india,
+                format(dtaa_tax_rate_pc, ".2f"),
 
             f"Amount{space}": int_credit_claimed,
             "Credit claimed under section 91": 0,

commit f14848b75f9f16e5f60463bcebde3576463acf92

foreign_sched_values_calc: Fix error due to incomplete withholding key.
Fixes the following error:
	Traceback (most recent call last):
	  File "...", line 4858, in <module>
	    sys.exit(main())
	             ~~~~^^
	  File "...", line 4852, in main
	    create_schedule_fsi_and_form_67(avg_tax_rate)
	    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^
	  File "...", line 4741, in create_schedule_fsi_and_form_67
	    add_to_form67(*args)
	    ~~~~~~~~~~~~~^^^^^^^
	  File "...", line 4655, in add_to_form67
	    withholding_tax_rate_pc = country_attr("tax_withholding_rate_percent")
	  File "...", line 4652, in country_attr
	    return getattr(country, f"{attr}_{income_type_key}")
	AttributeError: 'Country' object has no attribute 'tax_withholding_rate_percent_dividend'. Did you mean: 'tax_withholding_rate_percent_for_dividend'?

Fixes: 07c305f87fb4 ("foreign_sched_values_calc: Fix form67 values for withholding > dtaa.")
Signed-off-by: Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>

foreign_sched_values_calc/foreign_sched_values_calc.py
@@ -4652,7 +4652,9 @@ def create_schedule_fsi_and_form_67(avg_tax_rate: Fraction) -> None:
             return getattr(country, f"{attr}_{income_type_key}")
 
         dtaa_tax_rate_pc = country_attr("dtaa_tax_rate_percent")
-        withholding_tax_rate_pc = country_attr("tax_withholding_rate_percent")
+        withholding_tax_rate_pc = country_attr(
+            "tax_withholding_rate_percent_for"
+        )
 
         int_tax_in_india = round_rs(income_value * avg_tax_rate)
         int_tax_dtaa = round_rs(income_value * dtaa_tax_rate_pc / 100)